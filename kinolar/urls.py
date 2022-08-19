0ad08d06d952b682d95f51b7dd88b9127e3f8171
Otabek <otabekrahmonovtest@gmail.com>
2022-08-19 09:29:56 +0500

    search

--- kinolar/urls.py
+++ kinolar/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.MoviesView.as_view(), name='movies-list'),
     path("filter/", views.FilterMoviesView.as_view(), name="filter"),
+    path("search/", views.Search.as_view(), name="search"),
     path("add-rating/", views.AddStarRating.as_view(), name='add-rating'),
     path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie-detail'),
     path('review/<int:pk>/', views.AddReview.as_view(), name='add-review'),
